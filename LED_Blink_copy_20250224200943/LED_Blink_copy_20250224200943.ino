011634ec397b885d4ae202ee9a8c3efdcb6df7f6
mbouc36 <mbouc36@gmail.com>
2025-03-01 17:26:32 -0500

    PWM RGB Color Flashing

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,35 +1,25 @@
-const byte ledPins[] = {15, 2, 0, 4, 5, 18, 19, 21, 22, 23}; //define LED pins
-const byte chns[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // define the pwm channels
-const int dutys[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                     1023, 512, 256, 128, 64, 32, 16, 8, 4, 2,
-                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
-                     }; // define the pwm dutys
+// This code uses PWM to control the color of an RGB LED
 
-int ledCounts; //led counts
-int delayTimes = 50; // flowing speed, the smaller, the faster
 
+const byte ledPins[] = {4, 2, 15}; // define red gree and blue pins
+const byte chns[] = {0, 1, 2}; // define pwm channels
+int red, green, blue;
 void setup() {
-  ledCounts = sizeof(ledPins);
-  for (int i = 0; i < ledCounts; i++){
-    ledcAttachChannel(ledPins[i], 1000, 10, chns[i]); // attach each LED pin
+  for (int i = 0; i < 3; i++ ){ // set the pwm channels, one for each led, 1kHz, 8 bit
+    ledcAttachChannel(ledPins[i], 1000, 8, chns[i]);
   }
 }
 
 void loop() {
-  // flowing one side to the other R to L
-  for (int i = 0; i < 20; i++){
-    // loop 0 - 19
-    for (int j = 0; j < ledCounts; j++){ // loop through each LED 15-23
-      ledcWrite(ledPins[j], dutys[i + j]);
-    }
-    delay(delayTimes);
-  }
+  red = random(0, 256);
+  green = random(0, 256);
+  blue = random(0, 256);
+  setColor(red, green, blue);
+  delay(200);
+}
 
-  // flowing opposite direction L to R
-  for (int i = 0; i < 20; i++ ){
-    for (int j = ledCounts - 1; j > -1; j--){
-      ledcWrite(ledPins[j], dutys[i + (ledCounts -1 - j)]);
-    }
-    delay(delayTimes);
-  }
+void setColor(byte r, byte g, byte b){
+  ledcWrite(ledPins[0], 255 - r); 
+  ledcWrite(ledPins[1], 255 - g);
+  ledcWrite(ledPins[2], 255 - b);
 }
