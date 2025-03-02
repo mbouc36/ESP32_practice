23c293e5292c1057c9c5f44a5530e2a7174c4440
mbouc36 <mbouc36@gmail.com>
2025-03-01 19:52:51 -0500

    Door bell using passibe buzzer

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,15 +1,31 @@
 #define PIN_BUZZER 13
 #define PIN_BUTTON 4
+#define CHN        0 //define PWM channel
 
 void setup() {
-  pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+
+  //PWM config
+  ledcAttachChannel(PIN_BUZZER, 1, 10, CHN); //attach led pin to PWM channel
+  ledcWriteTone(PIN_BUZZER, 2000); // sound at 2kHz for 0.3s, this function generates a square wave with duty cycle 50%
+  delay(300);
 }
 
 void loop() {
   if (digitalRead(PIN_BUTTON) == LOW){
-    digitalWrite(PIN_BUZZER, HIGH);
+    alert();
   } else{
-    digitalWrite(PIN_BUZZER, LOW);
+    ledcWriteTone(PIN_BUZZER, 0);
   } 
+}
+
+void alert(){
+  float sinVal; //variable to store sine value
+  int toneVal; // variable to store sound frequency
+  for (int x = 0; x < 360; x += 10){ //x from 0 -> 360 degrees
+    sinVal = sin(x * (PI / 100));
+    toneVal = 2000 + sinVal * 500;
+    ledcWriteTone(PIN_BUZZER, toneVal);
+    delay(10);
+  }
 }
