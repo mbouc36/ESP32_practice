4c2cc53a361297ee1ea38cbba63adc8b116853c0
mbouc36 <mbouc36@gmail.com>
2025-03-15 20:48:46 -0400

    Serial read

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,10 +1,34 @@
+String inputString = ""; // a String to hold incoming data
+bool stringComplete = false; // whether the string complete
 
 void setup(){
   Serial.begin(115200); //begin and set baude rate 
-  Serial.println("ESP32 initilization Completed!");
+  Serial.println(String(\n"ESP32 initilization Completed! \n")) ;
 }
 
 void loop(){
   Serial.printf("Running time: %.1f s\n", millis() / 1000.0f);
   delay(1000);
+}
+
+String inputString = ""; //a String to hold incoming data bool stringComplete = false; // whether the string is complete
+void setup() {
+  Serial.begin(115200);
+  Serial.println(String("\nESP32 initialization completed! \n")
+}
+void loop() {
+  if (Serial.available()) {
+    char inChar = Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+  if (stringComplete) {
+// judge whether data has been received
+     // read one character
+
+  inputString = "";
+  stringComplete = false;
+}
 }
