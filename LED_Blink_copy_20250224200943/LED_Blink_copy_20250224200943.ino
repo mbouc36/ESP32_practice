302943025708fe772eb23e648cfa4109a7615f35
mbouc36 <mbouc36@gmail.com>
2025-03-15 21:59:56 -0400

    touch sensor

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,15 +1,9 @@
-#define PIN_ANALOG_IN 4
-
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop(){
-  int adcVal = analogRead(PIN_ANALOG_IN);
-  int dacVal = map(adcVal, 0, 4095, 0, 255); // converts range of 0-4095 to range of 0 to 255
-  double voltage = adcVal /4095.0 * 3.3;
-  dacWrite(DAC1, dacVal);
-  Serial.printf("ADC Val: %d, \t DAC Val: %d, \t Voltage: %.2fV\n", adcVal, dacVal, voltage);
-  delay(200);
+  Serial.printf("Touch value: %d \n", touchRead(T0)); //get value using T0 (GPIO4)
+  delay(1000);
 }
