a8b4f4bb712edd58032847aa63654666cd699313
mbouc36 <mbouc36@gmail.com>
2025-03-15 22:50:56 -0400

    photoresistor PWM LED

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,6 +1,8 @@
 #define PIN_ANALOG_IN 4
-#define PIN_LED 25
+#define PIN_LED 12
 #define CHAN 0
+#define LIGHT_MIN 372
+#define LIGHT_MAX 2048
 
 void setup() {
   ledcAttachChannel(PIN_LED, 1000, 12, CHAN);
@@ -8,7 +10,7 @@ void setup() {
 
 void loop(){
   int adcVal = analogRead(PIN_ANALOG_IN); //read adc value
-  int pwmVal = adcVal;                    //adcVal re-map to pwmVal
-  ledcWrite(PIN_LED, pwmVal);
+  int pwmVal = map(constrain(adcVal, LIGHT_MIN, LIGHT_MAX), LIGHT_MIN, LIGHT_MAX, 0, 4095);
+  ledcWrite(PIN_LED, pwmVal); //set pwm value
   delay(10);
 }
