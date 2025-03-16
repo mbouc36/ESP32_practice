9c9a9081adb4ba16c9794ba1767494a7dcc04545
mbouc36 <mbouc36@gmail.com>
2025-03-15 22:25:51 -0400

    Touch LED

--- LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
+++ LED_Blink_copy_20250224200943/LED_Blink_copy_20250224200943.ino
@@ -1,9 +1,31 @@
+#define PIN_LED 2
+#define PRESS_VAL 14
+#define RELEASE_VAL 25
 
+bool isProcessed = false;
 void setup() {
   Serial.begin(115200);
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop(){
-  Serial.printf("Touch value: %d \n", touchRead(T0)); //get value using T0 (GPIO4)
-  delay(1000);
+  if (touchRead(T0) < PRESS_VAL){
+    if (!isProcessed){
+      isProcessed = true; //avoid multiple detections
+      Serial.println("Touch detected! ");
+      reverseGPIO(PIN_LED);
+    }
+  }
+  
+  if (touchRead(T0) > RELEASE_VAL){
+    if (isProcessed){
+      isProcessed = false;
+      Serial.println("Released! ");
+    }
+  }
+}
+
+
+void reverseGPIO(int pin){
+  digitalWrite(pin, !digitalRead(pin));
 }
